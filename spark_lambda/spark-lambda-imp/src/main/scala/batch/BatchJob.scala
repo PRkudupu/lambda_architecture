4952a2fe033de2ba3b06c8267ea7b0a7d4d10c07
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2019-09-03 04:03:54 -0700

    spark read from text file was successful.

--- spark_lambda/spark-lambda-imp/src/main/scala/batch/BatchJob.scala
+++ spark_lambda/spark-lambda-imp/src/main/scala/batch/BatchJob.scala
@@ -1,9 +1,33 @@
 package batch
-package java.lang.management.ManagementFactory
 
+import java.lang.management.ManagementFactory
 
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by Ahmad Alkilani on 5/1/2016.
+ */
 object BatchJob {
-  def main (args:Array[String]):Unit ={
-    //
+  def main (args: Array[String]): Unit = {
+
+    // get spark configuration
+    val conf = new SparkConf()
+      .setAppName("Lambda with Spark")
+
+    // Check if running from IDE
+    if (ManagementFactory.getRuntimeMXBean.getInputArguments.toString.contains("IntelliJ IDEA")) {
+      System.setProperty("hadoop.home.dir", "c:\\WinUtils") // required for winutils
+      conf.setMaster("local[*]")
+    }
+
+    // setup spark context
+    val sc = new SparkContext(conf)
+    implicit val sqlContext = new SQLContext(sc)
+
+    // initialize input RDD
+    val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\data.tsv"
+    val input = sc.textFile(sourceFile)
+    input.foreach(println)
   }
 }
