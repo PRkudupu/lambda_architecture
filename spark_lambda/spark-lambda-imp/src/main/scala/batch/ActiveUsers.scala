4cf696fb767dec9fb8e16f0d4be0385ab50fa6a9
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2019-09-07 11:42:57 -0700

    write and read from parquet.

--- spark_lambda/spark-lambda-imp/src/main/scala/batch/ActiveUsers.scala
+++ spark_lambda/spark-lambda-imp/src/main/scala/batch/ActiveUsers.scala
@@ -14,45 +14,22 @@ object ActiveUsers {
     import sqlContext.implicits._
 
     // initialize input RDD
-    val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order_header.tsv"
-    val input = sc.textFile(sourceFile)
 
-    //Remove header
-    val header =input.first()
-    val rows = input.filter (line=>line != header)
+      val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order_header.tsv"
+      val input = sc.textFile(sourceFile)
 
-    //Read from csv and convert to parquet file
-    val rowsDF=rows.toDF()
-     writeParquet(rowsDF,"purchase_order_parquet")
-
-    //val parquetSourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order_parquet"
-    //val parquetInput = sc.textFile(parquetSourceFile)
-     val inputDF = rows.flatMap { line =>
-      //split and assign it to a variable
-      val record = line.split("\\t")
-      //check if all columns are present
-      if (record.length == 13){
-        //flatMap would unbox and return the activity
-        Some(domain.PurchaseOrder(record(0), record(1), record(2), record(3), record(4), record(5), record(6),record(7), record(8), record(9), record(10), record(11), record(12)))
-      }
-      else
-        None
-    }.toDF()
-
-    val df =inputDF.select(
-      //Function to add one month( Time_stamp_hour would be a month ahead.
-      inputDF("order_number"),inputDF("cosmos_customerid"),inputDF("header_purchase_date")
-    )
-    // register temp table
-    df.registerTempTable(tableName="purchase_order")
-
-    val visitorsByProduct=sqlContext.sql(
-      //triple quote is used for multiline code
-      """SELECT
-        * from purchase_order
-       """)
-    visitorsByProduct.show()
-
+      //val header = input.first()
+      //val rows = input.filter(line => line != header)
+   //write file in parquet format
+    writeParquet(input.toDF(),"purchase_order_parquet")
+    //Read parquet file
+    val parquetSourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order_parquet"
+    val parquetFile = sqlContext.read.parquet(parquetSourceFile)
+    //parquetFile.foreach(println)
+    //Register temp table
+    parquetFile.registerTempTable("purchase_order_tb")
+    val activeUsers=sqlContext.sql("select * from purchase_order_tb")
+    activeUsers.foreach(println)
   }
 
   /*
@@ -75,4 +52,6 @@ object ActiveUsers {
       .mode("overwrite")
       .save("C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\"+fileName)
     }
+
 }
+d
