bc739197c6323cb2de1192ebb342a269dd90b16a
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2019-09-07 11:18:20 -0700

    Load file and ignore header.

--- spark_lambda/spark-lambda-imp/src/main/scala/batch/ActiveUsers.scala
+++ spark_lambda/spark-lambda-imp/src/main/scala/batch/ActiveUsers.scala
@@ -7,38 +7,30 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object ActiveUsers {
   def main(args: Array[String]): Unit = {
-    // get spark configuration
 
-    val conf = new SparkConf()
-      .setAppName("Lambda with Spark")
-
-    // Check if running from IDE
-    if (ManagementFactory.getRuntimeMXBean.getInputArguments.toString.contains("IntelliJ IDEA")) {
-      System.setProperty("hadoop.home.dir", "c:\\WinUtils") // required for winutils
-      conf.setMaster("local[*]")
-    }
-
-    // setup spark context
-    val sc = new SparkContext(conf)
+    // Specify spark configuration and spark context
+    val sc = new SparkContext(init())
     implicit val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
 
     // initialize input RDD
-    val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order.tsv"
+    val sourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order_header.tsv"
     val input = sc.textFile(sourceFile)
 
-    //val parquetDF=input.toDF()
-    //writeParquet(parquetDF,"purchase_order_parquet")
-    input.foreach(println)
+    //Remove header
+    val header =input.first()
+    val rows = input.filter (line=>line != header)
 
-    //val inputDF = input.map { line =>
-    //flatMap expects a type that would unbox
-    val inputDF = input.flatMap { line =>
+    //Read from csv and convert to parquet file
+    val rowsDF=rows.toDF()
+     writeParquet(rowsDF,"purchase_order_parquet")
+
+    //val parquetSourceFile = "C:\\Boxes\\spark-kafka-cassandra-applying-lambda-architecture\\vagrant\\purchase_order_parquet"
+    //val parquetInput = sc.textFile(parquetSourceFile)
+     val inputDF = rows.flatMap { line =>
       //split and assign it to a variable
       val record = line.split("\\t")
-      //Milli second in hour. No of milli sec in hour. Used to convert the original time stamp to an hourly timestamp
-     //Validate if the actual input is of the actual length
-      println(record.length)
+      //check if all columns are present
       if (record.length == 13){
         //flatMap would unbox and return the activity
         Some(domain.PurchaseOrder(record(0), record(1), record(2), record(3), record(4), record(5), record(6),record(7), record(8), record(9), record(10), record(11), record(12)))
@@ -57,15 +49,26 @@ object ActiveUsers {
     val visitorsByProduct=sqlContext.sql(
       //triple quote is used for multiline code
       """SELECT
-        *
-        from purchase_order
-       """).cache()
+        * from purchase_order
+       """)
     visitorsByProduct.show()
 
   }
+
   /*
   Function to write parquet files
    */
+  def init(): SparkConf = {
+    val conf = new SparkConf()
+      .setAppName("Lambda with Spark")
+
+    // Check if running from IDE
+    if (ManagementFactory.getRuntimeMXBean.getInputArguments.toString.contains("IntelliJ IDEA")) {
+      System.setProperty("hadoop.home.dir", "c:\\WinUtils") // required for winutils
+      conf.setMaster("local[*]")
+    }
+   conf
+  }
   def writeParquet(dfName:DataFrame, fileName:String):Unit={
     dfName.write
       .option("header", "true")
